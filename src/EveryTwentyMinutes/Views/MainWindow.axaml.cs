4a21a51b712b327290245f3eb9b2c2f9a585c5ff
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-04 18:52:22 +0800

    feat(init): initial setup

---
+++ src/EveryTwentyMinutes/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EveryTwentyMinutes.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
