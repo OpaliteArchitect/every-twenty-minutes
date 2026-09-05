04402cc0674ba6305435d2131434eaa2abbd392a
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-05 23:52:14 +0800

    feat(mvp): implement in MVVM proper timer flow and UI states (#1)
    * add MainTimer View
    
    * remove MainTimer window, move app logic to MainWindow and MainViewModel
    
    * add Timer model
    
    * add Timer State enum, add proper state handling to ClickButton
    
    * complete ui state handling for buttons and timer ticks

---
+++ src/EveryTwentyMinutes/Models/Timer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using Avalonia.Threading;
+
+namespace EveryTwentyMinutes.Models;
+
+public class Timer
+{
+    private readonly DispatcherTimer _second = new() { Interval = TimeSpan.FromSeconds(1) };
+
+    public bool IsWorkMode { get; set; } = true; // updates at the start of the next mode
+    public enum State
+    {
+        Idle,
+        Running,
+        Paused,
+        Completed
+    }
+    public State CurrentState = State.Idle;
+    public int SecondsRemaining { get; set; }
+    public event Action? Tick;
+
+    public Timer()
+    {
+        _second.Tick += OnSecondTick;
+    }
+
+    private void OnSecondTick(object? sender, EventArgs e)
+    {
+        SecondsRemaining--;
+        if (SecondsRemaining is 0)
+        {
+            _second.Stop();
+            CurrentState = State.Completed;
+        }
+        Tick?.Invoke();
+    }
+
+    public void StartWork()
+    {
+        SecondsRemaining = 5; //20 * 60;
+        _second.Start();
+        IsWorkMode = true;
+        CurrentState = State.Running;
+    }
+
+    public void StartBreak()
+    {
+        SecondsRemaining = 3;
+        _second.Start();
+        IsWorkMode = false;
+        CurrentState = State.Running;
+    }
+
+    public void Resume()
+    {
+        _second.Start();
+        CurrentState = State.Running;
+    }
+
+    public void Pause()
+    {
+        _second.Stop();
+        CurrentState = State.Paused;
+    }
+}
--- src/EveryTwentyMinutes/ViewModels/MainViewModel.cs
+++ src/EveryTwentyMinutes/ViewModels/MainViewModel.cs
@@ -1,9 +1,100 @@
+using System;
+using System.Diagnostics;
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EveryTwentyMinutes.Models;
 
 namespace EveryTwentyMinutes.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string Time { get; set; } = "20:00";
+    [ObservableProperty]
+    public partial string Description { get; set; } = "Next break in";
+    [ObservableProperty]
+    public partial string ButtonText { get; set; } = "Start timer";
+
+    private readonly Timer _timer = new();
+
+    public MainViewModel()
+    {
+        _timer.Tick += OnSecondElapsed;
+    }
+
+    private void OnSecondElapsed()
+    {
+        Debug.WriteLine("elapsed");
+        int minutes = _timer.SecondsRemaining / 60;
+        int seconds = _timer.SecondsRemaining % 60;
+
+        Time = $"{minutes:d2}:{seconds:d2}";
+
+        if (_timer.SecondsRemaining is 0)
+        {
+            ShowCompletedUI();
+        }
+    }
+
+    private void ShowCompletedUI()
+    {
+        if (_timer.IsWorkMode)
+        {
+            Description = "Time for a break";
+            Time = "Look at something 20 feet away for 20 seconds.";
+            ButtonText = "Start looking";
+        }
+        else
+        {
+            Description = "Great job!";
+            Time = "Your 20 second break is complete. Confirm to resume the 20 minute interval timer.";
+            ButtonText = "Confirm";
+        }
+    }
+
+    [RelayCommand]
+    private void ClickButton()
+    {
+        switch (_timer.CurrentState)
+        {
+            case Timer.State.Idle when _timer.IsWorkMode:
+                _timer.StartWork();
+                Time = "20:00";
+                ButtonText = "Pause timer";
+                break;
+
+            case Timer.State.Idle when !_timer.IsWorkMode:
+                _timer.StartBreak();
+                Description = "Look 20 feet away";
+                Time = "00:20";
+                ButtonText = "Pause timer";
+                break;
+
+            case Timer.State.Paused:
+                _timer.Resume();
+                ButtonText = "Pause timer";
+                break;
+
+            case Timer.State.Running:
+                _timer.Pause();
+                ButtonText = "Resume timer";
+                break;
+
+            case Timer.State.Completed:
+                _timer.IsWorkMode = !_timer.IsWorkMode;
+                _timer.CurrentState = Timer.State.Idle;
+                if (_timer.IsWorkMode)
+                {
+                    Description = "Next break in";
+                    Time = "20:00";
+                    ButtonText = "Start timer";
+                }
+                else
+                {
+                    ClickButton();
+                }
+                break;
+        }
+    }
 }
