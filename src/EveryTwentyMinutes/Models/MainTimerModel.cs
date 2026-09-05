e57cd7f2e890f7060336149bdc794abae01d5afa
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-05 18:33:18 +0800

    add MainTimer View

---
+++ src/EveryTwentyMinutes/Models/MainTimerModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Avalonia.Threading;
+
+namespace EveryTwentyMinutes.Models;
+
+public class MainTimerModel
+{
+    public DispatcherTimer MainTimer;
+
+    public MainTimerModel()
+    {
+        TimeSpan twentyMinutes = new(0, 20, 0);
+        MainTimer = new()
+        {
+            Interval = twentyMinutes
+        };
+        MainTimer.Tick += MainTimerTick;
+    }
+
+    private void MainTimerTick(object? sender, EventArgs e)
+    {
+        if (sender is DispatcherTimer mainTimer)
+        {
+            
+        }
+    }
+}
---
+++ src/EveryTwentyMinutes/ViewModels/MainTimerViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EveryTwentyMinutes.ViewModels;
+
+public partial class MainTimerViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    public partial string MainTimer { get; set; } = "20:00";
+}
