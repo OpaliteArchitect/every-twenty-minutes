ac6755db5281770263b0473c96603f90926866b4
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-05 22:40:32 +0800

    add Timer model

---
+++ src/EveryTwentyMinutes/Models/TimerModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Avalonia.Threading;
+
+namespace EveryTwentyMinutes.Models;
+
+public class Timer
+{
+    private readonly DispatcherTimer _second = new() { Interval = TimeSpan.FromSeconds(1) };
+
+    public bool IsWorkMode { get; set; } // updates at the start of the next mode
+    public bool IsRunning => _second.IsEnabled;
+    public int SecondsRemaining { get; set; }
+    public event Action? Tick;
+
+    public Timer()
+    {
+        _second.Tick += OnSecondTick;
+    }
+
+    private void OnSecondTick(object? sender, EventArgs e)
+    {
+        SecondsRemaining--;
+
+        if (SecondsRemaining is 0)
+        {
+            Tick?.Invoke();
+            _second.Stop();
+        }
+    }
+
+    public void StartWork()
+    {
+        SecondsRemaining = 20 * 60;
+        _second.Start();
+        IsWorkMode = true;
+    }
+
+    public void StartBreak()
+    {
+        SecondsRemaining = 20;
+        _second.Start();
+        IsWorkMode = false;
+    }
+
+    public void Resume() => _second.Start();
+    
+    public void Pause() => _second.Stop();
+}
--- src/EveryTwentyMinutes/ViewModels/MainViewModel.cs
+++ src/EveryTwentyMinutes/ViewModels/MainViewModel.cs
@@ -1,15 +1,43 @@
+using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EveryTwentyMinutes.Models;
 
 namespace EveryTwentyMinutes.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Timer { get; set; } = "20:00";
+    public partial string Time { get; set; } = "20:00";
     [ObservableProperty]
     public partial string Description { get; set; } = "Next break in";
     [ObservableProperty]
-    public partial string ButtonText { get; set; } = "";
+    public partial string ButtonText { get; set; } = "Start timer";
 
+    private readonly Timer _timer = new();
 
+    public MainViewModel()
+    {
+        _timer.Tick += UpdateUI;
+    }
+
+    private void UpdateUI()
+    {
+        int minutes = _timer.SecondsRemaining / 60;
+        int seconds = _timer.SecondsRemaining % 60;
+        Time 
+    }
+
+    [RelayCommand]
+    private void ToggleTimer()
+    {
+        if (_timer.IsRunning)
+        {
+            _timer.Pause();
+        }
+        else
+        {
+            _timer.Resume();
+        }
+    }
 }
