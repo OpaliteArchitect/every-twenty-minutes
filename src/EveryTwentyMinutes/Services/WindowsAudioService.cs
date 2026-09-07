af19502e959677a2f4d118fca8e3e21021d41de4
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-07 13:03:47 +0800

    add beep notifications for windows

---
+++ src/EveryTwentyMinutes/Services/WindowsAudioService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EveryTwentyMinutes.Services;
+
+public class WindowsAudioService
+{
+    public async Task PlayDoubleBeep()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            await Task.Run(() => Console.Beep(800, 1000));
+            await Task.Delay(750);
+        }
+    }
+
+    public async Task PlaySingleBeep()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            await Task.Run(() => Console.Beep(1200, 500));
+            await Task.Delay(250);
+        }
+    }
+}
